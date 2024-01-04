8f466de4de24ddbad109211d84be51bea87d111d
jxcob <starus3@gmail.com>
2024-01-04 19:10:22 +0100

    nowosci

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "@/components/Button";
 import ButtonLink from "./ButtonLink";
+import ImageHandler from "./ImageHandler";
 
 const Navbar = () => {
   return (
     <div className="text-white">
       <ButtonLink to="Home">Home</ButtonLink>
+      <ImageHandler />
     </div>
   );
 };
